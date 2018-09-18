ce33564f9bf8e6de940ee7e053edb4ed0a7a234f
mike80by@gmail.com <your_email@whatever.com>
2018-09-18 13:42:41 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+ function logBase(num,base) {
+ 	return Math.log(num) / Math.log(base);
+ }
+ var countZ = 0;
+ for (let i = 1; i <= Math.floor(logBase(number, 5)); i++) {
+ 	countZ += Math.floor(number / Math.pow(5, i));
+ }
+ return countZ;
 }
